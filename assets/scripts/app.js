588a2427532b810cb852c2231b62b3a077f863ff
behappy23 <beharry.jason30@gmail.com>
2019-09-25 11:27:07 -0400

    Set up initial templates
    set up api urls, getFormFields, and prepared to deploy.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,5 +7,12 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
-})
+      // your JS code goes here
+      const getFormFields = require('<path to lib>/get-form-fields.js')
+
+      $('#create-book').on('submit', function(event) {
+        event.preventDefault()
+
+        const form = event.target
+        const bookData = getFormFields(form)
+      })
